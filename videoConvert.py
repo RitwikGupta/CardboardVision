8f512ae13f27d0f5aab059ba2eeacb51459a9de6
Knyte <rajatsemailaddress@gmail.com>
2015-02-07 02:19:26 -0500

    Untested implementation of videoConvert now up

--- videoConvert.py
+++ videoConvert.py
@@ -1,1 +1,30 @@
 # Module for taking a locally saved video and extracting audio and frames
+
+# written by rajat mehndiratta (github/knyte) at TartanHacks w2015
+# credit to Ayub Khan @ StackOverflow
+
+import subprocess
+import cv2
+
+def getAudio(videoFile, audioName)
+    assert(type(videoFile) == type(audioName) == str)
+    command = "ffmpeg -i " + videoFile + "- ab 160k ac 2 -ar 44100 -vn " + audioName + ".wav"
+    subprocess.call(command, shell=True)
+
+def getFrames(videoFile, namePrefix)
+    assert(type(videoFile) == str)
+    vc = cv2.VideoCapture(videoFile)
+    c = 1
+
+    if vc.isOpened():
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    while rval:
+        rval, frame = vc.read()
+        cv2.imwrite(str(namePrefix + str(c)) + ".png", frame)
+        c += 1
+        cv2.waitKey(1)
+
+    vc.release()
