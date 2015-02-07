0fc66be291a4d3e2db5bf2461f9c067acf39799a
Ritwik Gupta <ritwikg2004@live.com>
2015-02-07 03:51:20 -0500

    Fixed captureVideo.py

--- videoCapture.py
+++ videoCapture.py
@@ -2,16 +2,15 @@
 
 # created by rajat mehndiratta (github/knyte)
 
-#!/usr/bin/env python2
-
+import sys
 from pytube import YouTube
-from __future__ import print_function
+#from __future__ import print_function
 
 def getVideo(URL, sourceObject):
     filetypes = ['mp4', 'flv', 'webm', '3gp']
     resolutions = ['480p', '360p', '240p', '720p', '1080p', '144p']
-    for x in xrange(filetypes):
-        for y in xrange(resolutions):
+    for x in range(len(filetypes)):
+        for y in range(len(resolutions)):
             try:
                 video = sourceObject.get(x, y)
                 return video
@@ -22,8 +21,10 @@ def saveVideo(URL, directory, filename):
     yt = YouTube()
     yt.url = URL
     yt.filename = filename
-    video = getVideo(URL)
+    video = getVideo(URL, yt)
     if type(video) != None:
         video.download(directory)
     else:
         print "Pick another video! This one didn't work :("
+
+saveVideo(sys.argv[1], ".", "notPorn")
